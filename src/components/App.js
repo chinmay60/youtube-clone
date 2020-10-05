c32d037c33b89b7fb1a35963deb7212392aa7b3f
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-10-04 21:29:12 -0400

    minor fixes

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,21 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('New York');
+  }
+
   onTermSubmit = async (term) => {
     const response = await Youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo:response.data.items[0]
+
+    });
   };
 
   onVideoSelect = (video) => {
